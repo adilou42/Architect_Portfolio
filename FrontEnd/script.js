3187e8d747c8fea32b8b0148cdb14b047d59ff80
Adil Yakdi <yakdi.adil@gmail.com>
2024-01-16 17:07:38 +0100

    🔨 fetch data from api

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,33 @@
+const url = "http://localhost:5678/api/works";
+
+
+async function fetchData() {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        return data
+    } catch (error) {
+        console.error("Fetch error:", error);
+    }
+}
+
+async function displayData() {
+    const worksData = await fetchData()
+    console.log("worksData", worksData[2].imageUrl)
+    console.log("worksData", worksData[2].title)
+    console.log("worksData", worksData)
+
+    const newArray = worksData.map((work) => console.log("work", work))
+}
+
+displayData()
+
+
+
+
 function filterClick(event) {
     // Clear all buttons color
     const buttons = document.querySelectorAll('.filter-button');
